16909de45e04bd737a1eb9016cd4326a8444eb9a
nramana050 <110442371+nramana050@users.noreply.github.com>
2024-07-09 22:15:19 +0530

    Update App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chat from './components/Chat';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Chat with Documents</h1>
+      <Chat />
+    </div>
+  );
+};
+
+export default App;
